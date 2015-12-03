a45848de63c5cf5c698e3c861d6f4b4c9c03b200
Jemsann <jemma.touma@gmail.com>
2015-12-03 22:57:53 +0100

    Uppdaterat klass och vy för EducationPlan #10 och #2

--- PandaWeb/PandaWeb/Models/EducationPlan.cs
+++ PandaWeb/PandaWeb/Models/EducationPlan.cs
@@ -1,29 +1,34 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace PandaWeb.Models
 {
-    public class EducationPlan : IEnumerable<EducationPlan>
+    public class EducationPlan
     {
-        List<EducationPlan> epList = new List<EducationPlan>();
+        public EducationPlan(int id, string name, List<string> courses, DateTime startDate, DateTime endDate, string typeOfEducation)
+        {
+            ID = id;
+            Name = name;
+            Courses = courses;
+            StartDate = startDate;
+            EndDate = endDate;
+            TypeOfEducation = typeOfEducation;
+
+        }
+        
         public int ID { get; set; }
         public string Name { get; set; }
         public List<String> Courses { get; set; }
+
+        [DisplayFormat(DataFormatString="{0:yyyy/MM/dd}")]
         public DateTime StartDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
         public DateTime EndDate { get; set; }
         public string TypeOfEducation { get; set; }
-
-        public IEnumerator<EducationPlan> GetEnumerator()
-        {
-            return epList.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this.GetEnumerator();
-        }
     }
 }
