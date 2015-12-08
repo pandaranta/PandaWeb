d3b2251d875f557fb108522caff547710984e045
Jemsann <jemma.touma@gmail.com>
2015-12-08 10:21:06 +0100

    f

--- PandaWeb/PandaWeb/Controllers/HomeController.cs
+++ PandaWeb/PandaWeb/Controllers/HomeController.cs
@@ -30,7 +30,6 @@ namespace PandaWeb.Controllers
 
         public ActionResult EducationPlan()
         {
-            Console.WriteLine();
             ViewBag.Message = "Utbildningsplan";
             var id = 1;
             var name = "Systemutveckling";
