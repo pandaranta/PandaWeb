461e85e549d532d502b2bce53a2f231651aea80e
Jemsann <jemma.touma@gmail.com>
2015-12-10 13:22:14 +0100

    checka in backendutveckling-vy

--- PandaWeb/PandaWeb/Controllers/EducationController.cs
+++ PandaWeb/PandaWeb/Controllers/EducationController.cs
@@ -24,34 +24,34 @@ namespace PandaWeb.Controllers
 
         public ActionResult Backendutveckling()
         {
-            var eduPlan = new EducationPlan();
-            eduPlan.Name = "Backendutveckling";
-            eduPlan.EducationId = 2;
-            eduPlan.StartDate =
-                new DateTime(2015, 01, 01);
-            eduPlan.EndDate = new DateTime(2016, 12, 30);
+            //var eduPlan = new EducationPlan();
+            //eduPlan.Name = "Backendutveckling";
+            //eduPlan.EducationId = 2;
+            //eduPlan.StartDate =
+            //    new DateTime(2015, 01, 01);
+            //eduPlan.EndDate = new DateTime(2016, 12, 30);
 
-            context.EducationPlans.Add(eduPlan);
-            context.SaveChanges();
+            //context.EducationPlans.Add(eduPlan);
+            //context.SaveChanges();
             var backendutveckling = from e in context.EducationPlans
-                         where e.EducationId==2
+                         where e.Name=="Backendutveckling"
                          select e;
             return View(backendutveckling);
         }
 
         public ActionResult Systemutveckling()
         {
-            var eduPlan = new EducationPlan();
-            eduPlan.Name = "Systemutveckling";
-            eduPlan.EducationId = 3;
-            eduPlan.StartDate =
-                new DateTime(2015, 01, 01);
-            eduPlan.EndDate = new DateTime(2016, 12, 30);
+            //var eduPlan = new EducationPlan();
+            //eduPlan.Name = "Systemutveckling";
+            //eduPlan.EducationId = 3;
+            //eduPlan.StartDate =
+            //    new DateTime(2015, 01, 01);
+            //eduPlan.EndDate = new DateTime(2016, 12, 30);
 
-            context.EducationPlans.Add(eduPlan);
-            context.SaveChanges();
+            //context.EducationPlans.Add(eduPlan);
+            //context.SaveChanges();
             var sys  = from e in context.EducationPlans
-                       where e.EducationId == 3
+                       where e.Name=="Systemutveckling"
                        select e;
             return View(sys);
         }
