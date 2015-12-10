cc59d8108f49b934c78d60ceeb6deef4ad594ace
Jemsann <jemma.touma@gmail.com>
2015-12-10 12:50:41 +0100

    testat med att hårdkoda population av databasen

--- PandaWeb/PandaWeb/Controllers/EducationController.cs
+++ PandaWeb/PandaWeb/Controllers/EducationController.cs
@@ -24,9 +24,22 @@ namespace PandaWeb.Controllers
 
         public ActionResult EducationPlan()
         {
+            var eduPlan = new EducationPlan();
+            eduPlan.Name = "Backendutveckling";
+            eduPlan.EducationId = 2;
+            eduPlan.StartDate =
+                new DateTime(2015, 01, 01);
+            eduPlan.EndDate = new DateTime(2016, 12, 30);
+
+            context.EducationPlans.Add(eduPlan);
+            context.SaveChanges();
+            context.Database.Connection.Close();
+
+            context.Database.Connection.Open();
             var trying = from e in context.EducationPlans
                          orderby e.Name
                          select e;
+            context.Database.Connection.Close();
             return View(trying);
         }
 
---
+++ PandaWeb/PandaWeb/Models/PopulateDBWithEducationPlan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PandaWeb.Models
+{
+    public class PopulateDBWithEducationPlan
+    {
+        MyDBContext context = new MyDBContext();
+
+
+    }
+}
