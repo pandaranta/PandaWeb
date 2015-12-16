24f534753142033207784fbd4ffd610bb516610f
erk <rillokka@hotmail.com>
2015-12-16 01:35:58 +0100

    Massive update to multiple files.
    Added:
    Calendar and resources.
    Top menu links are now more consistent throughout shared views.
    Bootstrap.
    
    Removed:
    Junk actionlinks.
    Invalid references.

---
+++ PandaWeb/PandaWeb/Models/Appointment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DHTMLX.Scheduler;
+
+namespace PandaWeb.Models
+{
+    public class Appointment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DHXJson(Alias = "id")]
+        public int Id { get; set; }
+
+        [DHXJson(Alias = "text")]
+        public string Description { get; set; }
+
+        [DHXJson(Alias = "start_date")]
+        public DateTime StartDate { get; set; }
+
+        [DHXJson(Alias = "end_date")]
+        public DateTime EndDate { get; set; }
+    }
+}
